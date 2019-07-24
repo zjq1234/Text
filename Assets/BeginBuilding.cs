0e6fc1dec743dad65e28f6beea8d4c1af8058b1d
shao <123@qqcom>
2019-07-24 10:19:21 +0800

    Signed-off-by: shao <123@qqcom>

---
+++ Assets/BeginBuilding.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class BeginBuilding : MonoBehaviour,IPointerClickHandler
+{
+    public Canvas CanvasOfPlay;
+    public Canvas CanvasAfterLoading;
+    public Camera ARCamera;
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        CanvasOfPlay.gameObject.SetActive(true);
+        CanvasAfterLoading.gameObject.SetActive(false);
+        ARCamera.gameObject.SetActive(true);
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
