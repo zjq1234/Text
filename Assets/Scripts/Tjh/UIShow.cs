ed5d0c2b35e6cda524bfcf5033a5fa7f65da54c4
Tianjaihang <826319994@qq.com>
2019-07-25 11:46:42 +0800

    22

---
+++ Assets/Scripts/Tjh/UIShow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIShow : MonoBehaviour
+{
+    public GameObject Land;
+    public GameObject Register;
+
+
+    public void OnRegisterButtonClick()
+    {
+        Land.gameObject.SetActive(false);
+        Register.gameObject.SetActive(true);
+    }
+    public void OnReturnButtonClick()
+    {
+        Land.gameObject.SetActive(true);
+        Register.gameObject.SetActive(false);
+    }
+}
